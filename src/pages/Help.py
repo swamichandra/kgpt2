ea89bd6db9dff95d179250a26f607944725c48ae
Swami Chandrasekaran <swamichandra@gmail.com>
2023-04-27 13:52:30 -0500

    Update Help.py

--- src/pages/Help.py
+++ src/pages/Help.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 
 ##### Introduction
-image = Image.open('src\pages\howitworks.png')
+image = Image.open('src/pages/howitworks.png')
 st.image(image, use_column_width='always', width=50, caption='This app demonstrates how to use OpenAI\'s GPT-3 to answer questions on your own document, by using document embeddings and retrieval.')
 st.markdown(""" 
             
