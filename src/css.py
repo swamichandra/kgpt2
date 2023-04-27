05c1684a0427aa80ef3d7419899312121446c9ed
Swami Chandrasekaran <swamichandra@gmail.com>
2023-04-27 17:20:05 -0500

    Update css.py

--- src/css.py
+++ src/css.py
@@ -3,7 +3,19 @@ v1 = """
 
 html, body, [class*="css"] {
     font-family: 'IBM Plex Sans Condensed', sans-serif;
-    color: #091747;
 }
+
+div.stButton > button {
+        
+        cursor:pointer;outline:0;display:inline-block;font-weight:400;line-height:1.5;text-align:center;background-color:transparent;border:2px solid transparent;padding:6px 12px;font-size:1rem;border-radius:.25rem;transition:color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;color:#bf470a;border-color:#bf470a; display:block; margin: 0 auto;
+        
+    }
+    div.stButton > button:hover {
+        color:#fff;background-color:#bf470a;border-color:#bf470a
+    }
+
+    .small-font {
+        font-size: 9px !important;
+    }
 
 """
